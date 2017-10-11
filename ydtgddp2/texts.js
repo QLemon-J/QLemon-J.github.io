8eb20a7cf6fbb0ef45596906a56a60496d83d966
Mac <Mac@xp.local>
2017-10-11 17:15:10 +0800

    add

---
+++ ydtgddp2/texts.js
@@ -0,0 +1,117 @@
+﻿var g_strings =
+{
+	"ru":
+	{
+		"SELECT_BOOSTER": "Выберите усилители",
+		"PRICE": "Цена",
+		"SPLASH_TIME": "Время взрывать конфеты!",
+		"TASK_REMOVE_CANDIES": "Сотрите нужное количество конфет",
+		"TASK_CLEAR_DIRT": "Очистите поле от грязных ячеек",
+		"TASK_SAVE_STRAWBERRY": "Опустите все клубнички к низу поля",
+		"TASK_SCORE": "Наберите заданное количество очков",
+		"POWERUP_CELL": "Тапните на любую конфету, чтобы стереть ее",
+		"LOADING": "Загрузка",
+		"NOT_ENOUGH_GOLD": "Недостаточно золота",
+	},	
+
+	"en":
+	{
+		"SELECT_BOOSTER": "Select boosters",
+		"PRICE": "Price",
+		"SPLASH_TIME": "Splash Time!",
+		"TASK_REMOVE_CANDIES": "Remove a specified number of candies",
+		"TASK_CLEAR_DIRT": "Clear all dirty cells",
+		"TASK_SAVE_STRAWBERRY": "Bring down all strawberries",
+		"TASK_SCORE": "Gain specified score",
+		"POWERUP_CELL": "Tap any candy to remove it",
+		"LOADING": "Loading",
+		"NOT_ENOUGH_GOLD": "You dont have enough money",
+	},
+	
+	"es":
+	{
+		"SELECT_BOOSTER": "Seleccionar mejoras",
+		"PRICE": "Precio",
+		"SPLASH_TIME": "¡Es el momento de salpicar!",
+		"TASK_REMOVE_CANDIES": "¡Elimina un número especificado de dulces!",
+		"TASK_CLEAR_DIRT": "Despeja todas las celdas sucias",
+		"TASK_SAVE_STRAWBERRY": "Acaba con todas las fresas",
+		"TASK_SCORE": "Gana una puntuación especificada",
+		"POWERUP_CELL": "Cargando...",
+		"LOADING": "Pulsa en cualquier dulce para eliminarlo",
+		"NOT_ENOUGH_GOLD": "¡No hay dinero suficiente!",
+	},
+
+	"it":
+	{
+		"SELECT_BOOSTER": "Seleziona booster",
+		"PRICE": "Prezzo",
+		"SPLASH_TIME": "Tempo di Splash!",
+		"TASK_REMOVE_CANDIES": "Rimuovi un numero specificato di caramelle!",
+		"TASK_CLEAR_DIRT": "Elimina tutte le celle sporche",
+		"TASK_SAVE_STRAWBERRY": "Butta giù tutte le fragole",
+		"TASK_SCORE": "Ottieni il punteggio specificato",
+		"POWERUP_CELL": "Caricamento in corso...",
+		"LOADING": "Tocca qualsiasi caramella per rimuoverla",
+		"NOT_ENOUGH_GOLD": "Non abbastanza soldi!",
+	},
+
+	"pt":
+	{
+		"SELECT_BOOSTER": "Selecionar propulsores",
+		"PRICE": "Preço",
+		"SPLASH_TIME": "Tempo Principal!",
+		"TASK_REMOVE_CANDIES": "Remover um número especificado de doces!",
+		"TASK_CLEAR_DIRT": "Limpar todas as células sujas",
+		"TASK_SAVE_STRAWBERRY": "Trazer para baixo todos os morangos",
+		"TASK_SCORE": "Ganhar pontuação especificada",
+		"POWERUP_CELL": "A carregar…",
+		"LOADING": "Toque em qualquer doce para o remover",
+		"NOT_ENOUGH_GOLD": "Sem dinheiro suficiente!",
+	},
+
+	"tr":
+	{
+		"SELECT_BOOSTER": "Güçlendiricileri seç",
+		"PRICE": "Fiyat",
+		"SPLASH_TIME": "Sıçrama Zamanı!",
+		"TASK_REMOVE_CANDIES": "Belirli sayıda şekeri yok et!",
+		"TASK_CLEAR_DIRT": "Bütün kirli hücreleri temizle",
+		"TASK_SAVE_STRAWBERRY": "Bütün çilekleri kaldır",
+		"TASK_SCORE": "Belirli bir skor kazan",
+		"POWERUP_CELL": "Yükleniyor...",
+		"LOADING": "Kaldırmak için herhangi bir şekere tıkla",
+		"NOT_ENOUGH_GOLD": "Yeterli para yok!",
+	},
+
+	"fr":
+	{
+		"SELECT_BOOSTER": "Sélectionnez les rappels",
+		"PRICE": "Prix",
+		"SPLASH_TIME": "Moment de faire un plouf !",
+		"TASK_REMOVE_CANDIES": "Enlever un nombre spécifié de bonbons !",
+		"TASK_CLEAR_DIRT": "Nettoyer toutes les cellules sales",
+		"TASK_SAVE_STRAWBERRY": "Faire tomber toutes les fraises",
+		"TASK_SCORE": "Gagner un score spécifié",
+		"POWERUP_CELL": "Chargement en cours...",
+		"LOADING": "Appuyez sur n'importe quel bonbon pour le retirer",
+		"NOT_ENOUGH_GOLD": "Pas assez d'argent",
+	},
+
+	"de":
+	{
+		"SELECT_BOOSTER": "Booster wählen",
+		"PRICE": "Preis",
+		"SPLASH_TIME": "Splash!",
+		"TASK_REMOVE_CANDIES": "Entfernt eine bestimmte Anzahl Süßigkeiten",
+		"TASK_CLEAR_DIRT": "Säubert alle Felder",
+		"TASK_SAVE_STRAWBERRY": "Hole die Erbeeren nach unten!",
+		"TASK_SCORE": "Erreiche eine bestimmte Punktzahl",
+		"POWERUP_CELL": "Lädt...",
+		"LOADING": "Eine Süßigkeit berühren, um sie entfernen.",
+		"NOT_ENOUGH_GOLD": "Nicht genug Geld!",
+	},
+	
+};
+
+var g_allLanguages = [ "en", "es", "de", "fr", "pt", "it", "tr", "ru" ]; //language selection wiht correct turkish abreviation
